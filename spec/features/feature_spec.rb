bee363bb089262d123d8ab786611b1f7194cd351
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-01 18:20:53 +0000

    add hp feature with tests

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,16 +1,28 @@
 
 
 describe Battle, :type => :feature do
-  it "should return \"Let's battle!\"" do
-    visit'/'
-    expect(page).to have_content("Let's battle!")
+  feature "Name input" do
+    it "should return \"Let's battle!\"" do
+      visit'/'
+      expect(page).to have_content("Let's battle!")
+    end
+
+    it "should allow 2 players to enter names, submit, store as param and show their names" do
+      visit'/'
+      fill_in :player_one, with: 'Rory'
+      fill_in :player_two, with: 'Albert'
+      click_button('Fight!')
+      expect(page).to have_content("Player One: Rory Vs Player Two: Albert")
+    end
   end
 
-  it "should allow 2 players to enter names, submit, store as param and show their names" do
-    visit'/'
-    fill_in :player_one, with: 'Rory'
-    fill_in :player_two, with: 'Albert'
-    click_button('Fight!')
-    expect(page).to have_content("Player One: Rory Vs Player Two: Albert")
+  feature "Hit points" do
+    it "should show a player's hit points" do
+      visit'/'
+      fill_in :player_one, with: 'Rory'
+      fill_in :player_two, with: 'Albert'
+      click_button('Fight!')
+      expect(page).to have_content("HP: 100")
+    end
   end
 end
