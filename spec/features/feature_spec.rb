8ef7d65ff085c4394d944e21423ca457f6e449fe
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-03 10:23:59 +0000

    reduction in hp displays on attack page

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -24,5 +24,6 @@ describe Battle, :type => :feature do
     sign_in_and_play
     click_button("Attack!")
     expect(page).to have_content("Dave attacked Mittens!")
+    expect(page).to have_content("Mittens has HP90 remaining.")
   end
 end
